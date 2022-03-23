ae3c27181e65fb48894fb5b11492face1c7738b9
yubmun <89332480+yubmun@users.noreply.github.com>
2022-03-23 15:35:57 +0900

    22

---
+++ src/Ex2_1.java
@@ -0,0 +1,12 @@
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(5+3);
+		System.out.println(5-3);
+		System.out.println(5*3);
+		System.out.println(5/3);
+	}
+
+}

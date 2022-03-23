25ee113712e4b6efce82f0c63f9eb7ce1b518f3c
yubmun <89332480+yubmun@users.noreply.github.com>
2022-03-23 15:08:42 +0900

    first commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/20/60add47d48aa001c1ca2cccbd780224d
@@ -0,0 +1,11 @@
+
+public class Ex2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+	}
+
+}
---
+++ ch01/src/Hello.java
@@ -0,0 +1,25 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ctrl+shift+L 단축기 전체목록 보기
+		// ctrl + + , - 폰트 크기 증가/ 축소
+		// ctrl + D 한줄 삭제
+		// ctrl + alt + down 행단위 복사
+		// alt + shift + A 멀티컬럼 편집
+		// alt + up, down 행단위 이동
+		// 들여쓰기 tab , 내어쓰기 shift + tab , ctrl + i 자동 들여쓰기
+		// ctrl + / 주석처리 Toggle
+		// ctrl + spacebar 자동완성
+		System.out.println("따옴표 안에는 주석처리가 안됨");
+		System.out.println("Hello1234568 World");
+		System.out.println("Hello1234568 World");
+		System.out.println("Hello1234568 World");
+		System.out.println("Hello1234568 World");
+		System.out.println("Hello1234568 World");
+		System.out.println("Hello1234568 World");
+		System.out.println("Hello1234568 World");
+	}
+
+}
